a9d995e630f7e9d0a05dd945dec5488b6c212312
Ilya Karasev <97747062+faermot@users.noreply.github.com>
2024-10-14 18:34:03 +0300

    Добавьте файлы проекта.

---
+++ WpfApp21/View/Task2Page.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp21.Services;
+
+namespace WpfApp21.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Task2Page.xaml
+    /// </summary>
+    public partial class Task2Page : Page
+    {
+        public Task2Page()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTask3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnTask1_Click(object sender, RoutedEventArgs e)
+        {
+            ServiceNavigateHelper.FrameNavigate.Navigate(new Task1Page());
+          //  ServiceNavigateHelper.FrameNavigate.GoBack();
+        }
+    }
+}
---
+++ WpfApp21/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using WpfApp21.Services;
+using WpfApp21.View;
+
+namespace WpfApp21
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ServiceNavigateHelper.FrameNavigate = MainWindowFrame;
+
+            MainWindowFrame.Navigate(new Task1Page());
+        }
+    }
+}
